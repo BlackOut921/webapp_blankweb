17cefeee9165b2b609187b1999c52f0dce88915d
BlackOut 921 <conorhowe921@gmail.com>
2024-07-13 23:29:57 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_blankweb.Controllers
+{
+	[AllowAnonymous]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index() => View();
+	}
+}
---
+++ wwwroot/JavaScript.js
@@ -0,0 +1,14 @@
+﻿const _themeSwitch = document.querySelector(".bo-theme-switch-input");
+const _themeStoreName = "bo-theme";
+let _themeGet = localStorage.getItem(_themeStoreName);
+
+document.documentElement.setAttribute("data-theme", _themeGet ? _themeGet : "light");
+_themeSwitch.checked = _themeGet === "dark" ? true : false;
+
+window.addEventListener("load", () => {
+    _themeSwitch.addEventListener("change", () => {
+        let _theme = _themeSwitch.checked ? "dark" : "light";
+        document.documentElement.setAttribute("data-theme", _theme);
+        localStorage.setItem(_themeStoreName, _theme);
+    });
+});
