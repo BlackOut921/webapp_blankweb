059ba4af3942e7dbacd5b8c7cee16ccab2b0a620
BlackOut 921 <conorhowe921@gmail.com>
2024-07-15 13:30:16 +0100

    Theme into functions

--- wwwroot/JavaScript.js
+++ wwwroot/JavaScript.js
@@ -1,14 +1,48 @@
-﻿const _themeSwitch = document.querySelector(".bo-theme-switch-input");
-const _themeStoreName = "bo-theme";
-let _themeGet = localStorage.getItem(_themeStoreName);
+﻿//Find theme wwitch
+const _themeSwitch = document.querySelector(".bo-theme-switch-input");
+//Save theme storage name
+const _themeStorageName = "bo-theme";
+//Store current theme
+let _theme = "light";
 
-document.documentElement.setAttribute("data-theme", _themeGet ? _themeGet : "light");
-_themeSwitch.checked = _themeGet === "dark" ? true : false;
+//Get Preference (return=light|dark)
+function GetThemePreference() {
+    //Get localStorage value
+    const _storageValue = localStorage.getItem(_themeStorageName);
+    //Return either: _storageTheme or matchMedia query
+    return _storageValue ?
+        _storageValue :
+        window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
 
-window.addEventListener("load", () => {
+//Set preference (localStorage)
+function SetThemePreference(s) {
+    localStorage.setItem(_themeStorageName, s);
+    UpdateTheme();
+}
+
+//Update UI with new theme
+function UpdateTheme() {
+    document.documentElement.setAttribute("data-theme", _theme);
+    _themeSwitch.checked = _theme === "dark" ? true : false;
+}
+
+//Set theme early
+_theme = GetThemePreference();
+UpdateTheme();
+
+window.onload = () => {
+    UpdateTheme();
+    _themeSwitch.addEventListener("change", () => {
+        _theme = _theme === "light" ? "dark" : "light";
+        SetThemePreference(_theme);
+    });
+};
+
+/*window.addEventListener("load", () => {
     _themeSwitch.addEventListener("change", () => {
         let _theme = _themeSwitch.checked ? "dark" : "light";
         document.documentElement.setAttribute("data-theme", _theme);
-        localStorage.setItem(_themeStoreName, _theme);
+        localStorage.setItem("bo-theme", _theme);
     });
-});
+});*/
